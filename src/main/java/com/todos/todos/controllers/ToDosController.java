7e15612faec3708e2b664ad9618f5cacd83aba0c
Hector Garcia <hectorgarx2@gmail.com>
2025-06-26 21:12:20 -0600

    CRUD completed

--- src/main/java/com/todos/todos/controllers/ToDosController.java
+++ src/main/java/com/todos/todos/controllers/ToDosController.java
@@ -3,18 +3,34 @@ package com.todos.todos.controllers;
 import com.todos.todos.models.ToDo;
 import com.todos.todos.services.ToDoRepository ;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ToDosController {
     @Autowired
     ToDoRepository  toDoRepository;
 
+    @PostMapping("/todos")
+    ToDo create(@RequestBody ToDo todo){
+        return toDoRepository.save(todo);
+    }
+
     @GetMapping("/todos")
     List<ToDo> read(){
         return toDoRepository.findAllByOrderByIdAsc();
     }
+
+    @PutMapping("/todos")
+    ToDo update(@RequestBody ToDo todo){
+        return toDoRepository.save(todo);
+    }
+
+    @DeleteMapping("/todos/{id}")
+    void delete(@PathVariable Integer id){
+        toDoRepository.deleteById(id);
+    }
+
 }
